2f74c910f4cdccc9a7d66978e7acb74f5c353853
TBalo <tbalog426@gmail.com>
2022-08-28 17:36:26 +0100

    Added the wordUnscrambler Project

---
+++ WordUnscambler/Data/MatchedWord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordUnscambler.Data
+{
+    public struct MatchedWord
+    {
+        public string ScrambledWord { get; set; }
+        public string Word { get; set; }
+
+    }
+}
+
